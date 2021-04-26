3ad6c0bd8f324941e1a03affccd6829571684373
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2021-04-26 20:30:36 +0600

    1

--- main.js
+++ main.js
@@ -13,8 +13,19 @@ mousemovemethod = function (e) {
         
  }
 $('.testTracker').on('mousemove', mousemovemethod);
-$('#testTracker2').hover(
-    document.getElementsByClassName("slideLeft1").style.left = 0
-    // function(){ $(this).addClass('slideLeft') }
-    // function(){ $(this).removeClass('slideLeft') }
-    )
+
+var direction = "",
+oldx = 0,
+mousemovemethod2 = function (e) {
+        
+    if (e.pageX < oldx) {
+         direction = "left"
+    } else if (e.pageX > oldx) {
+       direction = "right"
+    }
+        document.getElementsById('newTest').innerHTML += '<div class="afterHoverBox slideLeft" id="testTracker1"><div class="contentBody d-flex justify-content-center align-items-center"><div class="row"><div class="col-12"><div class="title"><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id facere expedita quisquam maiores.</p></div></div><div class="col-12"><div class="links"><a href="" class="btn text-decoration-none btn1" > Click here</a><a href="" class="btn text-decoration-none btn1" > Buy Now</a></div></div></div></div></div>"'
+    
+     oldx = e.pageX;
+        
+ }
+$('#newTest').on('mousemove', mousemovemethod2);
